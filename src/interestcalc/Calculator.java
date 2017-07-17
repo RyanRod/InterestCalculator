ac5f23d67d61e777a09965747966ee33654e9c85
MRDIAZJR <rob_oxford@msn.com>
2017-07-17 15:14:32 -0400

    Pretending to be done.

--- src/interestcalc/Calculator.java
+++ src/interestcalc/Calculator.java
@@ -1,5 +1,14 @@
+/*Practice Java programming to compute interest earned by a savings account
+ * with a known yearly interest rate.
+ * 
+ *  input:  
+ *  
+ * 
+ * 
+ * */
+
 package interestcalc;
-// collab project by Ryan Rodriguez and M. Robert Diaz Jr.
+// collaboration project by Ryan Rodriguez and M. Robert Diaz Jr.
 
 
 public class Calculator {
