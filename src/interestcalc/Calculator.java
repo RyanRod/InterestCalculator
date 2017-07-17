b5baf4fd8b540f2a4819f6c2e612ec2c1ca4a405
Ryan Rodriguez <skyhawkrr@gmail.com>
2017-07-17 17:13:31 -0400

    Added input function

--- src/interestcalc/Calculator.java
+++ src/interestcalc/Calculator.java
@@ -8,24 +8,39 @@
  * */
 
 package interestcalc;
+
+import java.util.Scanner;
+
 // collaboration project by Ryan Rodriguez and M. Robert Diaz Jr.
 
 
 public class Calculator {
 
-	private double loanAmount;
-	private double interestRate;
-	
-	
+	private static double loanAmount = 100;
+	private static double interestRate = 1.1;
+	private static double years = 1;
 	
 	public static void main(String args[]){
+		GatherData();			
 		
-			
 	}
 	
-	public static void GatherData(){
+	private static void GatherData(){ // Asks user for info needed and sets it to globals
+		Scanner s = new Scanner(System.in);
 		
+		System.out.println("What is the amount of your principle? (Loan amount)");
+		loanAmount = Double.parseDouble(s.nextLine());
+		
+		System.out.println("What is the interest rate? (%)");
+		interestRate = Double.parseDouble(s.nextLine());
+		
+		System.out.println("How long will this interest accumulate? (In days)");
+		years = Double.parseDouble(s.nextLine()) / 365;
+		
+		System.out.println("Perfect!");
+		s.close();
 	}
+	
 	
 	
 }
